e2d13dab2fd5cecba0b1e05d16d59b8eb5ae6e5d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 18:11:36 -0300

    Ajust width

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rounded-xl gradient-bg flex-1 mx-2 lg:my-2 lg:mx-0">
+  <div class="w-full rounded-xl gradient-bg mx-2 lg:my-2 lg:mx-0 ">
     <div class="p-6">
       <div class="py-4"></div>
       <div class="font-bold text-xl text-white mb-1">Frontend Mentor</div>
