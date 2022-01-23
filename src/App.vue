6182eb96fb98fc5271c29bb3668e4619a1f0eab4
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 18:11:49 -0300

    Ajust justification

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,14 @@ import TitleCard from './components/TitleCard.vue'
 </script>
 
 <template>
-  <div class="px-10 py-12">
-    <div class="flex lg:flex-col lg:w-72">
+  <div class="px-10 py-12 h-screen bg-zinc-200 flex flex-col lg:flex-row">
+    <div class="flex lg:flex-col lg:w-72 justify-between lg:justify-start">
       <TitleCard />
       <TitleCard />
       <TitleCard />
       <!-- <FiltersCard /> -->
       <!-- <RoadmapCard /> -->
     </div>
+    <div class="flex w-full h-full bg-white"></div>
   </div>
 </template>
