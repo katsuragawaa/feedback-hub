5a6f48c7afc7f9e77bb36a5c8df792ec5e6020d3
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 18:28:10 -0300

    Fix justification

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full rounded-xl gradient-bg mx-2 lg:my-2 lg:mx-0 ">
+  <div class="w-full flex flex-col justify-end rounded-xl gradient-bg lg:my-2">
     <div class="p-6">
-      <div class="py-4"></div>
+      <div class="h-max"></div>
       <div class="font-bold text-xl text-white mb-1">Frontend Mentor</div>
       <div class="text-neutral-100">Feedback Board</div>
     </div>
