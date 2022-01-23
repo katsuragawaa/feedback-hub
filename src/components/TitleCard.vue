92a9681dc86478c30367aa60927104704d76cac9
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 17:59:02 -0300

    Ajust pading

--- src/components/TitleCard.vue
+++ src/components/TitleCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="rounded-xl gradient-bg flex-1 mx-2 lg:my-2 lg:mx-0">
-    <div class="px-8 py-10">
-      <div class="py-2"></div>
-      <div class="font-bold text-xl text-white mb-2">Frontend Mentor</div>
-      <div class="text-base text-neutral-100">Feedback Board</div>
+    <div class="p-6">
+      <div class="py-4"></div>
+      <div class="font-bold text-xl text-white mb-1">Frontend Mentor</div>
+      <div class="text-neutral-100">Feedback Board</div>
     </div>
   </div>
 </template>
