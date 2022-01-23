322c9c3448b1fae3dc3ba0f98e786ec84e911e99
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 18:28:23 -0300

    Create filters card

---
+++ src/components/FiltersCard.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const filters = ['All', 'UI', 'UX', 'Enhancement', 'Bug', 'Feature']
+</script>
+
+<template>
+  <div class="w-full rounded-xl bg-white mx-4 lg:my-2 lg:mx-0">
+    <div class="p-6 flex flex-wrap">
+      <div v-for="filter in filters" class="rounded-lg py-1 px-3 mr-4 mb-2 w-fit">{{ filter }}</div>
+    </div>
+  </div>
+</template>
