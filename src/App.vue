850b9bbb5649dfecc060a71fc18547658c2a478a
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 18:28:36 -0300

    Use filters card

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import TitleCard from './components/TitleCard.vue'
+import FiltersCard from './components/FiltersCard.vue'
 </script>
 
 <template>
   <div class="px-10 py-12 h-screen bg-zinc-200 flex flex-col lg:flex-row">
     <div class="flex lg:flex-col lg:w-72 justify-between lg:justify-start">
       <TitleCard />
+      <FiltersCard />
       <TitleCard />
-      <TitleCard />
-      <!-- <FiltersCard /> -->
       <!-- <RoadmapCard /> -->
     </div>
     <div class="flex w-full h-full bg-white"></div>
