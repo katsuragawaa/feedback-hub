bacab69e5cbe7a2ade138de42c6876469f8f52b9
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-23 17:47:20 -0300

    Add responsive cards

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TitleCard from './components/TitleCard.vue'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="px-10 py-12">
+    <div class="flex lg:flex-col lg:w-64">
+      <TitleCard />
+      <TitleCard />
+      <TitleCard />
+      <!-- <FiltersCard /> -->
+      <!-- <RoadmapCard /> -->
+    </div>
+  </div>
 </template>
-
-<style>
-#app {
-  font-family: Inter, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TitleCard.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="rounded-xl gradient-bg flex-1 mx-2 lg:my-2 lg:mx-0">
+    <div class="px-8 py-10">
+      <div class="py-2"></div>
+      <div class="font-bold text-xl text-white mb-2">Frontend Mentor</div>
+      <div class="text-base text-neutral-100">Feedback Board</div>
+    </div>
+  </div>
+</template>
