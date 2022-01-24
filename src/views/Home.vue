4ceaf4095d8bbb00cb57580caa868af41f75d72c
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 09:25:58 -0300

    Ajust padding

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@ import MenuCards from '../components/menu-cards/MenuCards.vue'
 </script>
 
 <template>
-  <div class="px-10 py-12 h-screen bg-zinc-200 flex flex-col lg:flex-row">
+  <div class="px-10 lg:px-36 py-14 h-screen bg-zinc-200 flex flex-col lg:flex-row">
     <MenuCards />
   </div>
 </template>
