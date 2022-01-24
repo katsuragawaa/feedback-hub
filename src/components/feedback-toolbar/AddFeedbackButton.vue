9c7913e06774b9dce41f8f7fc9df732dd1f99e4d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 14:12:54 -0300

    Add button router

--- src/components/feedback-toolbar/AddFeedbackButton.vue
+++ src/components/feedback-toolbar/AddFeedbackButton.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
+import router from '../../router'
+
+const newFeedback = () => router.push({ name: 'Home' })
 </script>
 
 <template>
   <button class="bg-purple-600 py-2 px-6 rounded-lg">
-    <span class="text-sm font-bold">Add Feedback</span>
+    <span class="text-sm font-bold" @click="newFeedback">Add Feedback</span>
   </button>
 </template>
