55618c39fd836972f5a889697ff6601243762d7a
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 14:04:24 -0300

    Create the button to add feedback and style better

---
+++ src/components/feedback-toolbar/AddFeedbackButton.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button class="bg-purple-600 py-2 px-6 rounded-lg">
+    <span class="text-sm font-bold">Add Feedback</span>
+  </button>
+</template>
--- src/components/feedback-toolbar/FeedbackToolbar.vue
+++ src/components/feedback-toolbar/FeedbackToolbar.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
 import SortBySelector from './SortBySelector.vue'
+import AddFeedbackButton from './AddFeedbackButton.vue';
 </script>
 
 <template>
-  <div class="w-full p-6 bg-[#373f68] rounded-xl text-white flex">
-    <img src="../../assets/suggestions/icon-suggestions.svg" />
-    <span class="font-bold">7 Suggestions</span>
-    <SortBySelector />
+  <div class="w-full p-6 bg-[#373f68] rounded-xl text-white flex justify-between">
+    <div class="flex items-center">
+      <img class="w-7 h-7" src="../../assets/suggestions/icon-suggestions.svg" />
+      <span class="font-bold text-lg px-6 ">7 Suggestions</span>
+      <SortBySelector />
+    </div>
+    <AddFeedbackButton />
   </div>
 </template>
