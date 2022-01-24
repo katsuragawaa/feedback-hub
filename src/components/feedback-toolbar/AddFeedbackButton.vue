5597ecc3715de8a2f47e4da78c9f46fb17286aca
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 14:38:44 -0300

    Add new route and fix click event location

--- src/components/feedback-toolbar/AddFeedbackButton.vue
+++ src/components/feedback-toolbar/AddFeedbackButton.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import router from '../../router'
 
-const newFeedback = () => router.push({ name: 'Home' })
+const newFeedback = () => router.push({ name: 'NewFeedback' })
 </script>
 
 <template>
-  <button class="bg-purple-600 py-2 px-6 rounded-lg">
-    <span class="text-sm font-bold" @click="newFeedback">Add Feedback</span>
+  <button class="bg-purple-600 py-2 px-6 rounded-lg" @click="newFeedback">
+    <span class="text-sm font-bold">Add Feedback</span>
   </button>
 </template>
