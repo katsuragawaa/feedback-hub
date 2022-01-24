910ccc011876573689667de9c0e0e1b0e366936f
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 09:45:56 -0300

    Create toolbar

---
+++ src/components/feedback-toolbar/FeedbackToolbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full p-6 bg-[#373f68] rounded-xl text-white">
+    <img src="../../assets/suggestions/icon-suggestions.svg" />
+  </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import MenuCards from '../components/menu-cards/MenuCards.vue'
+import FeedbackToolbar from '../components/feedback-toolbar/FeedbackToolbar.vue';
 </script>
 
 <template>
   <div class="px-10 lg:px-36 py-14 h-screen bg-zinc-200 flex flex-col lg:flex-row">
     <MenuCards />
+    <div class="h-10"></div>
+    <FeedbackToolbar />
   </div>
 </template>
