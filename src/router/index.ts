74c53aad0e55bb5cd9bea55b9b3f1d23b911e691
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 09:09:10 -0300

    Setup routes

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes: RouteRecordRaw[] = [{ path: '/', name: 'Home', component: Home }];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
