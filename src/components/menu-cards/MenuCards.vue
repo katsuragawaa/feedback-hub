fdf8e1ed3cecbabc807b3a3f9f8e13fc3fa25725
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 09:22:21 -0300

    Better file structure

---
+++ src/components/menu-cards/MenuCards.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TitleCard from './TitleCard.vue'
+import FiltersCard from './FiltersCard.vue'
+import RoadmapCard from './RoadmapCard.vue'
+</script>
+
+<template>
+  <div class="px-10 py-12 h-screen bg-zinc-200 flex flex-col lg:flex-row">
+    <div class="flex lg:flex-col lg:w-72 justify-between lg:justify-start">
+      <TitleCard />
+      <FiltersCard />
+      <RoadmapCard />
+    </div>
+  </div>
+</template>
