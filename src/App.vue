c82724fbbca247b2cfa9e80b83ed63c27bc3e5ee
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 09:11:53 -0300

    Add router view to App

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-import TitleCard from './components/TitleCard.vue'
-import FiltersCard from './components/FiltersCard.vue'
-import RoadmapCard from './components/RoadmapCard.vue'
-</script>
-
 <template>
-  <div class="px-10 py-12 h-screen bg-zinc-200 flex flex-col lg:flex-row">
-    <div class="flex lg:flex-col lg:w-72 justify-between lg:justify-start">
-      <TitleCard />
-      <FiltersCard />
-      <RoadmapCard />
-    </div>
-    <div class="flex w-full h-full bg-white"></div>
-  </div>
+  <router-view />
 </template>
