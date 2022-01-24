18078a5d863b6f8f391280ddfd420078f77fc14a
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 09:12:47 -0300

    Prettier

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../views/Home.vue'
 
-const routes: RouteRecordRaw[] = [{ path: '/', name: 'Home', component: Home }];
+const routes: RouteRecordRaw[] = [{ path: '/', name: 'Home', component: Home }]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
