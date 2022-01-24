33060ef7446ca370b4146aa95341492ebd04a8f8
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 13:29:18 -0300

    Add transition

--- src/components/feedback-toolbar/SortBySelector.vue
+++ src/components/feedback-toolbar/SortBySelector.vue
@@ -14,11 +14,16 @@ const sortOptions = {
 }
 
 const selectedOption = ref(sortOptions['upvotes-desc'])
+const dropdownVisibility = ref(false)
+
+const openDropdown = () => {
+  dropdownVisibility.value = true
+}
 
 const selectOption = (option: SortBy) => {
   selectedOption.value = option
+  dropdownVisibility.value = false
 }
-
 </script>
 
 <template>
@@ -40,6 +45,7 @@ const selectOption = (option: SortBy) => {
       <button
         class="inline-flex items-center justify-center w-full text-sm transition duration-150 ease-in-out hover:text-neutral-300"
         type="button"
+        @click="dropdownVisibility = !dropdownVisibility"
       >
         <div>
           Sort by :
@@ -48,13 +54,10 @@ const selectOption = (option: SortBy) => {
         <img src="../../assets/shared/white-arrow.svg" />
       </button>
 
-      <div
-        class="opacity-0 invisible dropdown-menu transition-all duration-300 transform origin-top-right -translate-y-2 scale-80"
-      >
+      <transition name="list">
         <div
+          v-show="dropdownVisibility"
           class="absolute left-0 w-52 mt-2 origin-top-right bg-white rounded-md shadow-lg"
-          aria-labelledby="headlessui-menu-button-1"
-          id="headlessui-menu-items-117"
           role="menu"
         >
           <div class="py-1">
@@ -73,16 +76,20 @@ const selectOption = (option: SortBy) => {
             </div>
           </div>
         </div>
-      </div>
+      </transition>
     </div>
   </div>
 </template>
 
-
 <style>
-.dropdown:focus-within .dropdown-menu {
-  opacity: 1;
-  transform: translate(0) scale(1);
-  visibility: visible;
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.3s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
 }
 </style>
