dca579db655b11b4edce3bb0e3ca100ff772a004
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-25 15:40:20 -0300

    Switch to declarative routing

--- src/components/feedback-toolbar/AddFeedbackButton.vue
+++ src/components/feedback-toolbar/AddFeedbackButton.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-import router from '../../router'
-
-const newFeedback = () => router.push({ name: 'NewFeedback' })
-</script>
-
 <template>
-  <button class="bg-purple-600 py-2 px-6 rounded-lg" @click="newFeedback">
-    <span class="text-sm font-bold">Add Feedback</span>
-  </button>
+  <router-link :to="{ name: 'NewFeedback' }">
+    <button class="bg-purple-600 py-2 px-6 rounded-lg">
+      <span class="text-sm font-bold">Add Feedback</span>
+    </button>
+  </router-link>
 </template>
