cd9cbe56440a0b405cff9bff9c908305ba254297
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-25 16:38:24 -0300

    Update tailwind config for small screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        md: { max: '760px' },
+      },
+    },
   },
   plugins: [],
 }
