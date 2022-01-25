7685926858c332f16c50564a1937ed01eb1038e6
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 21:14:09 -0300

    Add dummy feedback and pass as props

--- src/components/feedback-list/FeedbackCard.vue
+++ src/components/feedback-list/FeedbackCard.vue
@@ -1,28 +1,38 @@
 <script setup lang="ts">
+const { feedback } = defineProps<{
+  feedback: {
+    id: string,
+    title: string,
+    description: string,
+    type: string,
+    votes: number,
+    comments: number,
+  },
+}>()
 </script>
 
 <template>
-  <div class="flex justify-between bg-white rounded-xl p-8">
+  <div class="flex justify-between bg-white rounded-xl p-8 mb-4">
     <div class="flex">
       <div
         class="flex flex-col justify-between items-center h-14 w-11 py-3 px-2 bg-blue-600 bg-opacity-10 rounded-lg"
       >
         <img class="w-3" src="../../assets/shared/icon-arrow-up.svg" />
-        <div class="font-bold text-sm">13</div>
+        <div class="font-bold text-sm">{{ feedback.votes }}</div>
       </div>
       <div class="flex flex-col mx-10 cursor-pointer hover:text-blue-600 duration-300">
-        <div class="font-bold text-lg">More comprehensive reports</div>
-        <div class="text-gray-500 mt-2 mb-4">It would be great to see cool solutions.</div>
+        <div class="font-bold text-lg">{{ feedback.title }}</div>
+        <div class="text-gray-500 mt-2 mb-4">{{ feedback.description }}</div>
         <div
           class="rounded-lg py-2 px-6 w-fit bg-blue-600 font-medium text-sm text-blue-700 bg-opacity-20"
-        >Feature</div>
+        >{{ feedback.type }}</div>
       </div>
     </div>
     <div class="flex items-center justify-center">
       <div class="min-w-max">
         <img class="w-5" src="../../assets/shared/icon-comments.svg" />
       </div>
-      <div class="font-bold text-lg ml-3">2</div>
+      <div class="font-bold text-lg ml-3">{{ feedback.comments }}</div>
     </div>
   </div>
 </template>
--- src/components/feedback-list/FeedbackList.vue
+++ src/components/feedback-list/FeedbackList.vue
@@ -1,9 +1,44 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import FeedbackCard from './FeedbackCard.vue';
+
+type Feedback = {
+  id: string;
+  title: string;
+  description: string;
+  type: string;
+  votes: number;
+  comments: number;
+}
+
+const dummyFeedbacks = [
+  {
+    id: '1',
+    title: 'More comprehensive reports',
+    description: 'It would be great to see a more detailed breakdown of solutions.',
+    type: 'Feature',
+    votes: 123,
+    comments: 4,
+  },
+  {
+    id: '2',
+    title: 'Add tags for solutions',
+    description: 'Easier to search for solutions based on a specific stack.',
+    type: 'Enhancement',
+    votes: 23,
+    comments: 13,
+  },
+]
+
+const feedbacks = ref<Feedback[]>([])
+
+setTimeout(() => {
+  feedbacks.value = dummyFeedbacks
+}, 1000)
 </script>
 
 <template>
   <div class="mt-6">
-    <FeedbackCard />
+    <FeedbackCard v-for="feedback in feedbacks" :feedback="feedback" />
   </div>
 </template>
