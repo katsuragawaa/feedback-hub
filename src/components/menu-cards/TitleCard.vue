697a841a271c32948258256a01b351bb647a71e4
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 21:35:13 -0300

    Update title

--- src/components/menu-cards/TitleCard.vue
+++ src/components/menu-cards/TitleCard.vue
@@ -2,8 +2,8 @@
   <div class="w-full flex flex-col justify-end rounded-xl gradient-bg">
     <div class="p-6">
       <div class="my-10"></div>
-      <div class="font-bold text-lg text-white mb-1">Frontend Mentor</div>
-      <div class="text-sm text-neutral-100">Feedback Board</div>
+      <div class="font-bold text-lg text-white mb-1">Feedback Hub</div>
+      <div class="text-sm text-neutral-100">Give us your insight</div>
     </div>
   </div>
 </template>
