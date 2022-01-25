285936aaf5d3c0d6211b70f55ef4f818cae07492
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-24 21:30:06 -0300

    Fix min height

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@ import FeedbackList from '../components/feedback-list/FeedbackList.vue';
 </script>
 
 <template>
-  <div class="px-10 xl:px-36 py-14 bg-zinc-200 flex flex-col xl:flex-row">
+  <div class="px-10 xl:px-36 py-14 min-h-screen bg-zinc-200 flex flex-col xl:flex-row">
     <MenuCards />
     <div class="h-10"></div>
     <div class="flex flex-col xl:ml-7 w-full">
