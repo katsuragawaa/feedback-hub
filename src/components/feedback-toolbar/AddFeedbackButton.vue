a0384fc7e72d8745ee72731e363d55cdf6a06643
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-25 21:16:27 -0300

    Add highlight on hover

--- src/components/feedback-toolbar/AddFeedbackButton.vue
+++ src/components/feedback-toolbar/AddFeedbackButton.vue
@@ -1,6 +1,6 @@
 <template>
   <router-link :to="{ name: 'NewFeedback' }">
-    <button class="bg-purple-600 py-2 px-6 rounded-lg">
+    <button class="bg-purple-600 py-2 px-6 rounded-lg hover:brightness-125 duration-300">
       <span class="text-sm font-bold">Add Feedback</span>
     </button>
   </router-link>
