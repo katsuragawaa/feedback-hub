bf7ab813e0355968cbbde276f2b5f08d172dfe33
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-27 08:53:05 -0300

    Add image

--- src/components/feedback-list/FeedbackEmpty.vue
+++ src/components/feedback-list/FeedbackEmpty.vue
@@ -1,4 +1,7 @@
 <template>
+  <div>
+    <img src="../../assets/suggestions/illustration-empty.svg" />
+  </div>
   <div>No feedback of category "UX"</div>
   <div>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</div>
 </template>
