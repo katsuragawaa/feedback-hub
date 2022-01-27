88f17ee688c802a4d02e020a1a4cd8bb33a8617f
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-27 09:28:28 -0300

    Style component

--- src/components/feedback-list/FeedbackEmpty.vue
+++ src/components/feedback-list/FeedbackEmpty.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <img src="../../assets/suggestions/illustration-empty.svg" />
+  <div class="bg-white w-full rounded-xl px-6 py-28 flex flex-col items-center text-center">
+    <div>
+      <img class="w-36" src="../../assets/suggestions/illustration-empty.svg" />
+    </div>
+    <div class="mt-14 mb-6">
+      <span class="text-2xl font-bold text-gray-600">No feedback of category "UX"</span>
+    </div>
+    <div>
+      <span
+        class="text-gray-500 text-sm sm:text-base"
+      >Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</span>
+    </div>
   </div>
-  <div>No feedback of category "UX"</div>
-  <div>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</div>
 </template>
