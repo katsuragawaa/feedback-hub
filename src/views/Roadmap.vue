01cf1f32d6e917b30171f5b61116c200ef0b4aaf
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 19:51:40 -0300

    Rename import

--- src/views/Roadmap.vue
+++ src/views/Roadmap.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import RoadmapToolbarVue from '../components/roadmap-toolbar/RoadmapToolbar.vue';
+import RoadmapToolbar from '../components/roadmap-toolbar/RoadmapToolbar.vue';
 </script>
 
 <template>
   <div class="px-10 py-14 xl:px-36 md:p-0 min-h-screen bg-zinc-200 flex flex-col">
-    <RoadmapToolbarVue />
+    <RoadmapToolbar />
     <div>Cards</div>
   </div>
 </template>
