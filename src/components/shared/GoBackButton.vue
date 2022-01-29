16180c071df0c33e931b08ed33dac441d675cfb6
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 10:32:20 -0300

    Create go back button

---
+++ src/components/shared/GoBackButton.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'GoBackButton',
+  props: {
+    textColor: {
+      type: String,
+      default: 'white'
+    },
+    iconColor: {
+      type: String,
+      default: '#fff'
+    }
+  },
+  computed: {
+    textClass() {
+      return `text-${this.textColor} font-bold ml-3 text-xs sm:text-sm`
+    },
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center">
+    <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+      <path d="M6 9L2 5l4-4" :stroke="iconColor" stroke-width="2" fill="none" fill-rule="evenodd" />
+    </svg>
+    <div :class="textClass">Go back</div>
+  </div>
+</template>
