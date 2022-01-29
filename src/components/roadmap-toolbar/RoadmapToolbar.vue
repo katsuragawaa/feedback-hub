4ab62a163f9d503ea123bc5c91055428d5590f5d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 10:32:34 -0300

    Create roadmap toolbar

---
+++ src/components/roadmap-toolbar/RoadmapToolbar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AddFeedbackButton from '../shared/AddFeedbackButton.vue';
+import GoBackButton from '../shared/GoBackButton.vue';
+</script>
+
+<template>
+  <div
+    class="w-full h-fit p-6 bg-[#373f68] rounded-xl text-white flex justify-between items-center md:rounded-none"
+  >
+    <div class="flex flex-col">
+      <GoBackButton />
+      <div>Roadmap</div>
+    </div>
+    <AddFeedbackButton />
+  </div>
+</template>
