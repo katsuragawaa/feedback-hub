cf0b748aef5d5e3cf952a5e33937e2af9878240c
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 10:46:04 -0300

    Add hover state

--- src/components/shared/GoBackButton.vue
+++ src/components/shared/GoBackButton.vue
@@ -28,7 +28,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="flex items-center cursor-pointer" @click="goBack">
+  <div class="flex items-center cursor-pointer hover:underline" @click="goBack">
     <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
       <path d="M6 9L2 5l4-4" :stroke="iconColor" stroke-width="2" fill="none" fill-rule="evenodd" />
     </svg>
