56837dcef7a80d0bb0c99e4a1a55ba049f30bd23
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 10:35:59 -0300

    Make cursor pointer

--- src/components/shared/GoBackButton.vue
+++ src/components/shared/GoBackButton.vue
@@ -22,7 +22,7 @@ export default defineComponent({
 </script>
 
 <template>
-  <div class="flex items-center">
+  <div class="flex items-center cursor-pointer">
     <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
       <path d="M6 9L2 5l4-4" :stroke="iconColor" stroke-width="2" fill="none" fill-rule="evenodd" />
     </svg>
