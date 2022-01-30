4d3413d44a0a8ede02334c4f580a579a42e1c75d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 21:34:17 -0300

    Ajust position and get the image

--- src/components/feedback-form/FeedbackForm.vue
+++ src/components/feedback-form/FeedbackForm.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import NewFeedbackIcon from './icons/NewFeedbackIcon.vue';
 import GoBackButton from '../shared/GoBackButton.vue';
 import {} from 'vue';
 
@@ -13,16 +12,21 @@ const { title } = defineProps({
 
 <template>
   <div
-    class="px-10 py-14 xl:px-36 text-gray-600 md:px-6 min-h-screen bg-zinc-200 flex flex-col"
+    class="px-28 py-14 xl:px-36 text-gray-600 md:px-6 min-h-screen bg-zinc-200 flex flex-col items-center"
   >
-    <GoBackButton textColor="gray-600" iconColor="#4661E6" />
-    <div
-      class="flex flex-col bg-white rounded-xl px-11 pt-14 pb-11 mt-16 relative"
-    >
-      <div class="absolute left-11 -top-7">
-        <NewFeedbackIcon />
+    <div class="max-w-3xl min-w-[540px] md:min-w-full">
+      <GoBackButton textColor="gray-600" iconColor="#4661E6" />
+      <div
+        class="flex flex-col bg-white rounded-xl px-11 pt-14 pb-11 mt-16 relative"
+      >
+        <div class="absolute left-11 -top-7 md:-top-5 md:w-10">
+          <img
+            src="../../assets/shared/icon-new-feedback.svg"
+            alt="New feedback icon"
+          />
+        </div>
+        <div class="text-xl font-bold">{{ title }}</div>
       </div>
-      <div class="text-xl font-bold">{{ title }}</div>
     </div>
   </div>
 </template>
