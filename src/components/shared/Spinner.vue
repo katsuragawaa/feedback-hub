388e07c6e817eba906e7ff09a2917831e43f1668
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-30 15:09:16 -0300

    Ajust spinner

--- src/components/shared/Spinner.vue
+++ src/components/shared/Spinner.vue
@@ -6,7 +6,7 @@ const { size } = defineProps<{
 
 <template>
   <svg
-    :class="`animate-spin h-${size} w-${size} text-white`"
+    :class="`animate-spin h-${size} w-${size}`"
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
@@ -20,7 +20,7 @@ const { size } = defineProps<{
       stroke-width="4"
     />
     <path
-      class="opacity-75"
+      class="opacity-100"
       fill="currentColor"
       d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
     />
