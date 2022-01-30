4b9276f16946b72324ef9b45c1894bcd11740bf9
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-29 21:05:52 -0300

    Change into composition API

--- src/components/feedback-form/FeedbackForm.vue
+++ src/components/feedback-form/FeedbackForm.vue
@@ -1,37 +1,28 @@
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-import GoBackButton from '../shared/GoBackButton.vue';
+<script setup lang="ts">
 import NewFeedbackIcon from './icons/NewFeedbackIcon.vue';
+import GoBackButton from '../shared/GoBackButton.vue';
+import {} from 'vue';
 
-export default defineComponent({
-  name: 'FeedbackForm',
-  components: {
-    GoBackButton,
-    NewFeedbackIcon,
-  },
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
+const { title } = defineProps({
+  title: {
+    type: String,
+    required: true,
   },
 });
 </script>
 
 <template>
   <div
-    class="px-10 py-14 xl:px-36 text-gray-600 md:p-0 min-h-screen bg-zinc-200 flex flex-col"
+    class="px-10 py-14 xl:px-36 text-gray-600 md:px-6 min-h-screen bg-zinc-200 flex flex-col"
   >
     <GoBackButton textColor="gray-600" iconColor="#4661E6" />
-
     <div
       class="flex flex-col bg-white rounded-xl px-11 pt-14 pb-11 mt-16 relative"
     >
       <div class="absolute left-11 -top-7">
         <NewFeedbackIcon />
       </div>
-      <div>Create a feedback</div>
+      <div class="text-xl font-bold">{{ title }}</div>
     </div>
   </div>
 </template>
