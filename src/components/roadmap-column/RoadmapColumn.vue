acd9cff5f2342139e4c642a7c4f88e94be910a63
Katsuragawa <andre.katsuragawa@gmail.com>
2022-01-30 22:15:36 -0300

    Create basic column component

---
+++ src/components/roadmap-column/RoadmapColumn.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col">
+    <div class="flex flex-col">
+      <div class="text-bold">Section title (3)</div>
+      <div class="text-gray-700">Section description</div>
+    </div>
+    <div>Cards</div>
+  </div>
+</template>
--- src/views/Roadmap.vue
+++ src/views/Roadmap.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import RoadmapToolbar from '../components/roadmap-toolbar/RoadmapToolbar.vue';
+import RoadmapColumn from '../components/roadmap-column/RoadmapColumn.vue';
 </script>
 
 <template>
-  <div class="px-10 py-14 xl:px-36 md:p-0 min-h-screen bg-zinc-200 flex flex-col">
+  <div
+    class="px-10 py-14 xl:px-36 md:p-0 min-h-screen bg-zinc-200 flex flex-col"
+  >
     <RoadmapToolbar />
-    <div>Cards</div>
+
+    <div class="flex justify-between">
+      <RoadmapColumn />
+      <RoadmapColumn />
+      <RoadmapColumn />
+    </div>
   </div>
 </template>
