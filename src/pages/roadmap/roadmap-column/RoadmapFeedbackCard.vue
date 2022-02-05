f003ef5bb6927c7c82665fcd5331a653443be972
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-05 20:27:02 -0300

    Add more content

--- src/pages/roadmap/roadmap-column/RoadmapFeedbackCard.vue
+++ src/pages/roadmap/roadmap-column/RoadmapFeedbackCard.vue
@@ -1,4 +1,9 @@
 <template>
   <div class="h-2 w-full rounded-t-lg bg-purple-600"></div>
-  <div class="mb-4 flex flex-col rounded-b-lg bg-white p-6">Card</div>
+  <div class="mb-4 flex flex-col rounded-b-lg bg-white p-6">
+    <div>Planned</div>
+    <div>More comprehensive reports</div>
+    <div>It would be great to see a more detailed breakdown of solutions.</div>
+    <div>Feature</div>
+  </div>
 </template>
