a84023ebaf82beb7fdba33f7205878fc6092ef23
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-05 22:49:54 -0300

    Use length for title

--- src/pages/roadmap/roadmap-column/RoadmapColumn.vue
+++ src/pages/roadmap/roadmap-column/RoadmapColumn.vue
@@ -13,8 +13,10 @@ const { feedbacks } = defineProps<{
 <template>
   <div class="flex w-full flex-col">
     <div class="flex flex-col">
-      <div class="mb-2 font-bold text-gray-700">{{ title }} (3)</div>
-      <div class="mb-6 text-gray-600">{{ description }}</div>
+      <div class="mb-2 font-bold text-gray-700">
+        {{ title }} ({{ feedbacks.length }})
+      </div>
+      <div class="mb-6 text-gray-600 text-sm">{{ description }}</div>
     </div>
     <div class="flex flex-col">
       <RoadmapFeedbackCard v-for="feedback in feedbacks" :feedback="feedback" />
