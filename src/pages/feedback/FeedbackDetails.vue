236e577339de6d9de73942d3cdb8f88ed26727a6
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-06 17:43:22 -0300

    Add buttons to header

--- src/pages/feedback/FeedbackDetails.vue
+++ src/pages/feedback/FeedbackDetails.vue
@@ -1,1 +1,19 @@
-<template>Details</template>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import GoBackButton from '@/components/GoBackButton.vue';
+import EditFeedbackButton from '@/components/EditFeedbackButton.vue';
+
+const route = useRoute();
+console.log(route.params.id);
+</script>
+
+<template>
+  <div
+    class="flex min-h-screen flex-col items-center bg-zinc-200 px-28 py-14 text-gray-600 md:px-6 xl:px-36"
+  >
+    <div class="min-w-[540px] max-w-3xl md:min-w-full flex justify-between">
+      <GoBackButton textColor="gray-600" iconColor="#4661E6" />
+      <EditFeedbackButton />
+    </div>
+  </div>
+</template>
