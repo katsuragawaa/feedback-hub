0fa3501a837d4cfff360fd29be9e8a914e23476c
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-05 22:31:33 -0300

    Ajust positions

--- src/pages/roadmap/roadmap-column/RoadmapFeedbackCard.vue
+++ src/pages/roadmap/roadmap-column/RoadmapFeedbackCard.vue
@@ -1,18 +1,77 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { Feedback } from '../../../types';
 
 const { feedback } = defineProps<{
   feedback: Feedback;
 }>();
+
+const voted = ref(false);
+
+const vote = () => {
+  voted.value = !voted.value;
+
+  if (voted.value) {
+    feedback.votes++;
+  } else {
+    feedback.votes--;
+  }
+};
 </script>
 
 <template>
   <div class="h-2 w-full rounded-t-lg bg-purple-600"></div>
   <div class="mb-4 flex flex-col rounded-b-lg bg-white p-6">
-    <div>Planned</div>
-    <div>More comprehensive reports</div>
-    <div>It would be great to see a more detailed breakdown of solutions.</div>
-    <div>Feature</div>
-    {{ feedback }}
+    <div class="flex flex-col items-start">
+      <div>
+        <div
+          class="flex cursor-pointer flex-col duration-300 hover:text-blue-600"
+        >
+          <div class="text-base font-bold sm:text-lg">{{ feedback.title }}</div>
+          <div class="mt-2 mb-4 text-sm text-gray-500 sm:text-base">
+            {{ feedback.details }}
+          </div>
+          <div
+            class="w-fit rounded-lg bg-blue-600 bg-opacity-20 py-2 px-6 text-sm font-medium text-blue-700"
+          >
+            {{ feedback.category }}
+          </div>
+        </div>
+
+        <div class="mt-4 flex items-center justify-between">
+          <div
+            class="flex h-fit w-fit cursor-pointer flex-row items-center justify-between rounded-lg bg-blue-600 py-2 px-4 duration-200"
+            :class="voted ? 'bg-opacity-100 text-white' : 'bg-opacity-10'"
+            @click="vote"
+          >
+            <svg
+              class="duration-500"
+              :class="{ 'rotate-180': voted }"
+              width="10"
+              height="7"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 6l4-4 4 4"
+                :stroke="voted ? 'white' : 'rgb(37 99 235)'"
+                stroke-width="2"
+                fill="none"
+                fill-rule="evenodd"
+              />
+            </svg>
+            <div class="ml-3 text-sm font-bold">{{ feedback.votes }}</div>
+          </div>
+
+          <div class="flex items-center">
+            <div class="min-w-max">
+              <img class="w-5" src="@/assets/shared/icon-comments.svg" />
+            </div>
+            <div class="ml-3 text-lg font-bold">
+              {{ feedback.comments.length }}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
