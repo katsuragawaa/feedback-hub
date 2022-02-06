dd47cf0f732cbdf7c63038a18e045fea4b5c5e7d
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-06 17:50:55 -0300

    Fetch feedback on mounted

--- src/pages/feedback/FeedbackDetails.vue
+++ src/pages/feedback/FeedbackDetails.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
-import { useRoute } from 'vue-router';
 import GoBackButton from '@/components/GoBackButton.vue';
 import EditFeedbackButton from '@/components/EditFeedbackButton.vue';
+import FeedbackCard from '../../components/FeedbackCard.vue';
+
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+import { readFeedbackData } from '../../services/DatabaseService';
 
 const route = useRoute();
-console.log(route.params.id);
+
+onMounted(async () => {
+  const feedbackId = route.params.id;
+  const feedback = await readFeedbackData(feedbackId as string);
+  console.log(feedback);
+});
 </script>
 
 <template>
   <div
     class="flex min-h-screen flex-col items-center bg-zinc-200 px-28 py-14 text-gray-600 md:px-6 xl:px-36"
   >
-    <div class="min-w-[540px] max-w-3xl md:min-w-full flex justify-between">
-      <GoBackButton textColor="gray-600" iconColor="#4661E6" />
-      <EditFeedbackButton />
+    <div class="min-w-[540px] max-w-3xl md:min-w-full">
+      <div class="flex justify-between">
+        <GoBackButton textColor="gray-600" iconColor="#4661E6" />
+        <EditFeedbackButton />
+      </div>
+      <!-- <FeedbackCard /> -->
     </div>
   </div>
 </template>
