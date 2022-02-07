8de581b407acdb5ef1520feedda5c67e6a505389
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-06 21:06:42 -0300

    Add background and simple title

--- src/pages/feedback/comment-list/CommentList.vue
+++ src/pages/feedback/comment-list/CommentList.vue
@@ -1,1 +1,7 @@
-<template>Comments</template>
+<script setup lang="ts"></script>
+
+<template>
+  <div class="rounded-lg bg-white p-8">
+    <div class="font-bold">2 Comments</div>
+  </div>
+</template>
