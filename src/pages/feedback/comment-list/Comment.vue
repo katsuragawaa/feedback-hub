7014c2634cdc73e33b2eb02827a4a0a74186be87
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-06 21:25:05 -0300

    Add styling to comment

--- src/pages/feedback/comment-list/Comment.vue
+++ src/pages/feedback/comment-list/Comment.vue
@@ -1,1 +1,16 @@
-<template><div>A comment</div></template>
+<template>
+  <div class="flex pt-8 text-sm">
+    <div class="h-10 w-10 flex-none rounded-full bg-slate-400"></div>
+
+    <div class="ml-6 flex flex-col">
+      <div class="flex items-center justify-between font-bold">
+        <div>John Doe</div>
+        <div>Reply</div>
+      </div>
+      <div class="mt-4">
+        This would be awesome! It would be so helpful to see an overview of my
+        code in a way that makes it easy to spot where things could be improved.
+      </div>
+    </div>
+  </div>
+</template>
