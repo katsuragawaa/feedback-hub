757059b9d5575df360a3fd794702c0491b7a45c1
Katsuragawa <andre.katsuragawa@gmail.com>
2022-02-06 21:07:34 -0300

    Create comment component

--- src/pages/feedback/comment-list/CommentList.vue
+++ src/pages/feedback/comment-list/CommentList.vue
@@ -1,7 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Comment from './Comment.vue';
+</script>
 
 <template>
   <div class="rounded-lg bg-white p-8">
     <div class="font-bold">2 Comments</div>
+
+    <Comment />
   </div>
 </template>
